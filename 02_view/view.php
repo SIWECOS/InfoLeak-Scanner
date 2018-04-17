abc41b317e98ec74f626f7d7c0e8498945bbfff3
Yakup Ates <Yakup.Ates@rub.de>
2018-04-17 16:56:01 +0200

    deactivate filter

--- 02_view/view.php
+++ 02_view/view.php
@@ -109,6 +109,30 @@ class View{
                     $result['score'] = 60;
                 }
             }
+            /*
+              if ($cms_node->nodeName === "script") {
+              $result['finding'] = preg_replace("/\\n|\\t/", "",
+              $cms_node->nextSibling->nodeValue);
+              $result['testDetails'][0]['values']['node'] = $cms_node->nodeName;
+
+              if (strlen($result['finding']) > 100) {
+              $result['finding']  = substr($result['finding'], 0, 100);
+              $result['finding'] .= " [...]";
+              }
+              $result['testDetails'][0]['values']['node_content'] = $result['finding'];
+              } else {
+              $i = 0;
+
+              foreach($cms_node->attributes as $attr) {
+              $finding = $attr->name . " : " . $attr->value;
+
+              if ($i < $MAX_FINDING_OUT)
+              $i++;
+              else
+              break;
+              }
+
+            */
 
             if ($cms_node->nodeName === "script") {
                 $result['finding'] = preg_replace("/\\n|\\t/", "",
