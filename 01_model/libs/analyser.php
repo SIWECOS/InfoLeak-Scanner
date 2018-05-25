3f7c937247d8175207a2da0b71166e269baebb24
Yakup Ates <Yakup.Ates@rub.de>
2018-05-25 15:41:33 +0200

    add some vuln jquery versions

--- 01_model/libs/analyser.php
+++ 01_model/libs/analyser.php
@@ -684,12 +684,15 @@ class Analyser {
 
         /* http://domstorm.skepticfx.com/modules?id=529bbe6e125fac0000000003 */
         /* Vulnerable to Selector XSS with class Attribute ('. XSS_VECTOR') */
+        /* other: https://www.cvedetails.com/vulnerability-list/vendor_id-6538/Jquery.html */
+        /* https://snyk.io/test/npm/jquery/1.12.4?severity=high&severity=medium&severity=low */
         $vuln_jquery = array(
             '2.0.3', '2.0.2', '2.0.1', '2.0.0', '1.10.2', '1.10.1',
             '1.10.0', '1.9.1', '1.9.0', '1.8.3', '1.8.2', '1.8.1', '1.8.0',
             '1.7.2', '1.7.1', '1.7.0', '1.6.4', '1.6.3', '1.6.2', '1.6.1',
             '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.2',
-            '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3'
+            '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3',
+			'1.12.0', '3.0.0', '1.12.4', '2.1.4'
         );
 
         /* https://www.cvedetails.com/vulnerability-list/vendor_id-11858/Netease.html */
