e9b015841eed3e73e59d8ac4650bb6ec45e24633
Yakup Ates <Yakup.Ates@rub.de>
2018-10-02 11:13:35 +0200

    new version

--- 02_view/view.php
+++ 02_view/view.php
@@ -29,7 +29,7 @@ include __DIR__ . '/../01_model/libs/messages.php';
  * Returns JSON output of findings.
  */
 class View{
-    private $version = "1.0.0";
+    private $version = "1.0.1";
     private $model;
     private $controller;
     private $messages;
