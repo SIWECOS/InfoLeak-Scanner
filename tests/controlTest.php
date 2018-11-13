ae54fe9d6cc3678136a21079ea0b81facfa73ba4
Yakup Ates <Yakup.Ates@rub.de>
2018-11-13 13:51:09 +0100

    delete object when done

--- tests/controlTest.php
+++ tests/controlTest.php
@@ -1,3 +1,18 @@
     public function setUp() {
         $this->controller = new Control("siwecos.de", "");
     }
+    public function tearDown() {
+        unset($this->controller);
+
+        /*
+        $createdFiles = Array(
+            "/tmp/setHeader.txt",
+            "/tmp/callbackPostData.txt",
+            "/tmp/testGettingPostParameters.txt",
+        );
+        
+        foreach($createdFiles as $file) {
+            unlink($file);
+        }
+        */
+    }
