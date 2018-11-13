932e80e8e498d43498e1a730cf4e052b01ddfd66
Yakup Ates <Yakup.Ates@rub.de>
2018-11-13 13:51:27 +0100

    test set header

--- tests/controlTest.php
+++ tests/controlTest.php
@@ -16,3 +16,41 @@
         }
         */
     }
+
+    /**
+     * @dataProvider dataProviderSetHeader
+     */    
+    public function testSetHeader($data_encoded, $expectedResult) {
+        $data = json_decode($data_encoded);
+        
+        $controller = new Control($data->url, $data->userAgent);
+        $controller->setDangerLevel($data->dangerLevel);
+        $controller->setCallbackurls($data->callbackurls);
+        $controller->setUserAgent($data->userAgent);
+        
+        $model = new Model($controller);
+        $view = new View($model, $controller, "POST");
+
+        $this->assertFileExists("/tmp/setHeader.txt");
+        $content = file_get_contents("/tmp/setHeader.txt");
+        
+        $this->assertEquals($content,
+                            $expectedResult);
+    }
+
+    public function dataProviderSetHeader() {
+        $userAgent = "test";
+        
+        $json = json_encode (
+            array(
+                "url" => "siwecos.de",
+                "dangerLevel" => 0,
+                "callbackurls" => array("localhost/InfoLeak-Scanner/tests/testSetHeader.php"),
+                "userAgent" => $userAgent
+            )  
+        );
+        
+        return [
+            [$json, $userAgent]
+        ];
+    }
