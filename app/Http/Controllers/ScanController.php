382487ca1318dcc26c56b9c2a0f9d54958fc1322
Yakup Ates <Yakup.Ates@rub.de>
2019-06-14 16:29:47 +0200

    add user-agent support for GET requests

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -22,6 +22,9 @@ class ScanController extends Controller
             $request->get('userAgent', \Config::get('scanner.user_agent'))
         );
 
-        return response($scan->scan(), 200)->header('Content-Type', 'application/json; charset=utf-8');
+        return response($scan->scan(), 200)
+            ->header('Content-Type', 'application/json; charset=utf-8')
+            ->header('User-Agent', $request->get('userAgent', \Config::get('scanner.user_agent')));
+    }
     }
 }
